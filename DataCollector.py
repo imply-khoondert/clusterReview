a218a4a179b16e8040f4124de91d9a882fc763ec
Kyle Hoondert <khoondert@Kyle-Hoondert.local>
2020-06-17 10:24:53 +0100

    Start

---
+++ DataCollector.py
@@ -0,0 +1,144 @@
+import requests
+import argparse
+import sys
+import tempfile
+import zipfile
+import json
+import os
+import datetime
+
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+def checkOptions():
+    parser = argparse.ArgumentParser(description = 'Imply Cluster Review - Data Collector')
+    parser.add_argument('Customer', action='store', type=str, help='Enter a customer name')
+    parser.add_argument('-r', '--router', action='store', type=str, dest='router', help='Router URL with protocol and port')
+    parser.add_argument('-k', '--kerberos', action='store_true', dest='kerberos', help='''
+                        Enable kerberos authentication (must already have valid ticket''')
+    parser.add_argument('-a','--auth', action='store_true', dest='auth', help='Enable basic authentication')
+    parser.add_argument('-u', '--user', action='store', type=str, dest='user', help='Username')
+    parser.add_argument('-p', '--password', action='store', type=str, dest='password', help='Password')
+    parser.add_argument('-t', '--token', action='store', type=str, dest='token', help='Token (like YRtWa... - no "Basic"')
+    args = parser.parse_args()
+    return args
+
+def getRequest(URL):
+    fullURL = options.router + URL
+    if options.kerberos:
+        req = requests.get(fullURL, auth=kerberos_auth, verify=False)
+
+    elif options.auth:
+        headers = {'Authorization': 'Basic ' + options.token}
+        req = requests.get(fullURL, headers=headers, verify=False)
+
+    else:
+        req = requests.get(fullURL, verify=False)
+    try:
+        JSON = req.json()
+    except json.decoder.JSONDecodeError:
+        JSON = {}
+    return JSON
+    
+
+def postRequest(URL, query):
+    fullURL = options.router + URL
+    if options.kerberos:
+        headers = {'Content-Type': 'application/json'}
+        req = requests.post(fullURL, headers=headers, json=qery, auth=kerberos_auth, verify=False)
+
+    elif options.auth:
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Basic ' + options.token}
+        req = requests.post(fullURL, headers=headers, json=query, verify=False)
+    else:
+        headers = {'Content-Type': 'application/json'}
+        req = requests.post(fullURL, headers=headers, json=query, verify=False)
+
+    JSON = req.json()
+    return JSON
+
+
+def getSegments():
+    queryJSON = {'query': 'SELECT "datasource", count("segment_id") as segmentCount, AVG("size") as avgSize, avg("num_rows") as avgRows FROM sys.segments  GROUP BY "datasource"'}
+    URL = '/druid/v2/sql'
+    segmentJSON = postRequest(URL, queryJSON)
+    return segmentJSON
+
+
+def getCompaction():
+    URL = '/druid/coordinator/v1/config/compaction'
+    compactionJSON = getRequest(URL)
+    return compactionJSON
+
+
+def getSupervisors():
+    URL = '/druid/indexer/v1/supervisor?full'
+    supervisorsJSON = getRequest(URL)
+    return supervisorsJSON
+
+
+def getRetention():
+    URL = '/druid/coordinator/v1/rules?full'
+    retentionJSON = getRequest(URL)
+    return retentionJSON
+
+def getDatasources():
+    URL = '/druid/coordinator/v1/metadata/datasources?full'
+    datasourceJSON = getRequest(URL)
+    return datasourceJSON
+
+def getCoordinatorSettings():
+    URL = '/druid/coordinator/v1/config'
+    coordinatorJSON = getRequest(URL)
+    return coordinatorJSON
+
+def getOverlordSettings():
+    URL = '/druid/indexer/v1/worker'
+    overlordJSON = getRequest(URL)
+    return overlordJSON
+
+def main():
+    global options
+    options = checkOptions()
+
+    if options.router is None:
+        print('Please specify a druid Router like "-r https://localhost:8888"')
+        sys.exit(2)
+
+    if options.kerberos:
+        from requests_kerberos import HTTPKerberosAuth, REQUIRED
+        global kerberos_auth
+        kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, sanitize_mutual_error_response=False)
+
+    if options.auth:
+        if not ((options.user and options.password) or options.token):
+            print('Please enter username and password, or token')
+            sys.exit(2)
+        else:
+            if not options.token:
+                import base64
+                userPass = bytes(options.user + ':' + options.password, encoding='utf-8')
+                options.token = base64.b64encode(userPass).decode("utf-8")
+            
+
+    segmentJSON = getSegments()
+    compactionJSON = getCompaction()
+    supervisorsJSON = getSupervisors()
+    retentionJSON = getRetention()
+    datasourceJSON = getDatasources()
+    coordinatorJSON = getCoordinatorSettings()
+    overlordJSON = getOverlordSettings()
+    
+    fileDetails = options.Customer + '_' + datetime.date.today().isoformat()
+    with zipfile.ZipFile(fileDetails + '.zip', 'w', zipfile.ZIP_DEFLATED) as archive:
+        archive.writestr(os.path.join(fileDetails, 'segments.json'), json.dumps(segmentJSON))
+        archive.writestr(os.path.join(fileDetails, 'compaction.json'), json.dumps(compactionJSON))
+        archive.writestr(os.path.join(fileDetails, 'supervisors.json'), json.dumps(supervisorsJSON))
+        archive.writestr(os.path.join(fileDetails, 'retention.json'), json.dumps(retentionJSON))
+        archive.writestr(os.path.join(fileDetails, 'datasources.json'), json.dumps(datasourceJSON))
+        archive.writestr(os.path.join(fileDetails, 'coordinator.json'), json.dumps(coordinatorJSON))
+        archive.writestr(os.path.join(fileDetails, 'overlord.json'), json.dumps(overlordJSON))
+
+
+if __name__ == '__main__':
+    main()
---
+++ SheetGen.py
@@ -0,0 +1,144 @@
+import xlsxwriter
+import json
+import argparse
+import datetime
+import zipfile
+import os
+
+def checkOptions():
+    parser = argparse.ArgumentParser(description = 'Imply Cluster Review - XLSX Generator')
+    parser.add_argument('Customer', action='store', type=str, help='Enter a customer name')
+    parser.add_argument('-f', '--fileName', action='store', dest='filename', help='Enter the location of the collected datafile')    
+    args = parser.parse_args()
+    return args
+
+
+def segmentToSheet(ws, data):
+    ws.write(0, 0, "DataSource")
+    ws.write(0, 1, "Segment Count")
+    ws.write(0, 2, "Average Size")
+    ws.write(0, 3, "Average Rows")
+    sheetRow = 1
+    sheetCol = 0
+    for item in data:
+        ws.write(sheetRow, sheetCol, item['datasource'])
+        ws.write(sheetRow, sheetCol + 1, item['segmentCount'])
+        ws.write(sheetRow, sheetCol + 2, item['avgSize'])
+        ws.write(sheetRow, sheetCol + 3, item['avgRows'])
+        sheetRow += 1
+    return
+
+
+def compactionToSheet(ws, data):
+    ws.write(0, 0, "DataSource")
+    ws.write(0, 1, "Task Priority")
+    ws.write(0, 2, "Input Segment Size Bytes")
+    ws.write(0, 3, "Max Rows Per Segment")
+    ws.write(0, 4, "Skip Offset From Latest")
+    ws.write(0, 5, "Tuning Config")
+    ws.write(0, 6, "Task Context")
+    sheetRow = 1
+    sheetCol = 0
+    for item in data['compactionConfigs']:
+        ws.write(sheetRow, sheetCol, item['dataSource'])
+        ws.write(sheetRow, sheetCol + 1, item['taskPriority'])
+        ws.write(sheetRow, sheetCol + 2, item['inputSegmentSizeBytes'])
+        ws.write(sheetRow, sheetCol + 3, item['maxRowsPerSegment'])
+        ws.write(sheetRow, sheetCol + 4, item['skipOffsetFromLatest'])
+        ws.write(sheetRow, sheetCol + 5, item['tuningConfig'])
+        ws.write(sheetRow, sheetCol + 6, item['taskContext'])
+        sheetRow += 1
+    return
+
+
+def retentionToSheet(ws, data):
+    ws.write(0, 0, "DataSource")
+    ws.write(0, 1, "Rule")
+    ws.write(0, 2, "Period")
+    ws.write(0, 3, "Future")
+    ws.write(0, 4, "Tier")
+    ws.write(0, 5, "Replicas")
+    sheetRow = 1
+    sheetCol = 0
+    for ds, rules in data.items():
+        ws.write(sheetRow,sheetCol, ds)
+        sheetRow += 1
+        sheetCol += 1
+        for item in rules:
+            ws.write(sheetRow, sheetCol, item['type'])
+            try:
+                ws.write(sheetRow, sheetCol + 1, item['period'])
+                ws.write(sheetRow, sheetCol + 2, item['includeFuture'])
+                for tier, reps in item['tieredReplicants'].items():
+                    ws.write(sheetRow, sheetCol + 3, tier)
+                    ws.write(sheetRow, sheetCol + 4, reps)
+                    sheetRow += 1
+            except:
+                sheetRow += 1
+        sheetCol = 0
+
+    return
+
+
+def supervisorsToSheet(ws, data):
+    ws.write(0, 0, "DataSource")
+    ws.write(0, 1, "Stream")
+    ws.write(0, 2, "ID")
+    ws.write(0, 3, "Type")
+    ws.write(0, 4, "State")
+    ws.write(0, 5, "Task Count")
+    ws.write(0, 6, "Replicas")
+    ws.write(0, 7, "Task Duration")
+    ws.write(0, 8, "Records Per Fetch")
+    ws.write(0, 9, "Max rows in mem")
+    ws.write(0, 10, "Max rows per segment")
+    sheetRow = 1
+    sheetCol = 0
+    for item in data:
+        ws.write(sheetRow, sheetCol, item['spec']['spec']['dataSchema']['dataSource'])
+        ws.write(sheetRow, sheetCol + 1, item['spec']['ioConfig']['stream'])
+        ws.write(sheetRow, sheetCol + 2, item['id'])
+        ws.write(sheetRow, sheetCol + 3, item['spec']['type'])
+        ws.write(sheetRow, sheetCol + 4, item['detailedState'])
+        ws.write(sheetRow, sheetCol + 5, item['spec']['ioConfig']['taskCount'])
+        ws.write(sheetRow, sheetCol + 6, item['spec']['ioConfig']['replicas'])
+        ws.write(sheetRow, sheetCol + 7, item['spec']['ioConfig']['taskDuration'])
+        ws.write(sheetRow, sheetCol + 8, item['spec']['ioConfig']['recordsPerFetch'])
+        ws.write(sheetRow, sheetCol + 9, item['spec']['tuningConfig']['maxRowsInMemory'])
+        ws.write(sheetRow, sheetCol + 10, item['spec']['tuningConfig']['maxRowsPerSegment'])
+        sheetRow += 1
+    return
+
+
+def main():
+    global options
+    options = checkOptions()
+    
+    # Create an empty workbook
+    workbook = xlsxwriter.Workbook(options.Customer + '_' + datetime.date.today().isoformat() + '.xlsx')
+
+    zipBase = os.path.basename(options.filename)
+    zipDir = os.path.splitext(zipBase)[0]
+    
+    with zipfile.ZipFile(options.filename) as zipper:
+        with zipper.open(zipDir + '/' + 'segments.json') as segments:
+            segmentJSON = json.load(segments)
+            segmentSheet = workbook.add_worksheet('Segments')
+            segmentToSheet(segmentSheet, segmentJSON)
+        with zipper.open(zipDir + '/' + 'compaction.json') as compaction:
+            compactionJSON = json.load(compaction)
+            compactionSheet = workbook.add_worksheet('Compaction')
+            compactionToSheet(compactionSheet, compactionJSON)
+        with zipper.open(zipDir + '/' + 'retention.json') as retention:
+            retentionJSON = json.load(retention)
+            retentionSheet = workbook.add_worksheet('Retention')
+            retentionToSheet(retentionSheet, retentionJSON)
+        with zipper.open(zipDir + '/' + 'supervisors.json') as supervisors:
+            supervisorsJSON = json.load(supervisors)
+            supervisorsSheet = workbook.add_worksheet('Supervisors')
+            supervisorsToSheet(supervisorsSheet, supervisorsJSON)
+
+    workbook.close()
+
+if __name__ == '__main__':
+    main()
